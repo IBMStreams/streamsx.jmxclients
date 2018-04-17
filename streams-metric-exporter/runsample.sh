2ced9c32206ad8f0c0e097edb8ddc5db4c442ac8
Brian Williams <bmwilli1@comcast.net>
2018-04-16 21:07:23 -0400

    Modification to sample run script and add to release .tar

--- streams-metric-exporter/runsample.sh
+++ streams-metric-exporter/runsample.sh
@@ -1,1 +1,9 @@
-java -jar target/executable-streams-metric-exporter.jar -j service:jmx:jmxmp://host1:9975 -d StreamsDomain -i StreamsInstance -u userx -r 10 -p 25500 -h 0.0.0.0
+#!/bin/bash
+# Sample execution
+# If running inside of Streams Quickstart VM, use as is
+# If running on host (mac,pc) with Quickstart VM running, run the following command in the vmware image to get the 
+#    <quickstart vm ip>
+#    ifconfig eth0 
+#    Replace the localhost with the ip address returned
+JMXHOST=localhost
+java -jar target/executable-streams-metric-exporter.jar -j service:jmx:jmxmp://${JMXHOST}:9975 -d StreamsDomain -i StreamsInstance -u streamsadmin -r 10 -p 25500 -h 0.0.0.0
